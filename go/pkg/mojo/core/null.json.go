a6ec5e569d52634d85269b814a333435ccc5fbcd
Frankee.zhou <frankee.zhou@wayz.ai>
2021-11-26 08:41:49 +0800

    add null json codec

---
+++ go/pkg/mojo/core/null.json.go
@@ -0,0 +1,20 @@
+package core
+
+import (
+	jsoniter "github.com/json-iterator/go"
+	"unsafe"
+)
+
+func init() {
+	jsoniter.RegisterTypeEncoder("core.Null", &NullCodec{})
+}
+
+type NullCodec struct {
+}
+
+func (codec *NullCodec) Encode(ptr unsafe.Pointer, stream *jsoniter.Stream) {
+}
+
+func (codec *NullCodec) IsEmpty(ptr unsafe.Pointer) bool {
+	return true
+}
