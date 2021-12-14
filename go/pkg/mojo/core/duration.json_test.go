cf4b0e54659539b0601545d1b2cb72b638f0eae0
Frankee.zhou <frankee.zhou@gmail.com>
2021-12-14 16:09:35 +0800

    add extension to Duration

---
+++ go/pkg/mojo/core/duration.json_test.go
@@ -0,0 +1,20 @@
+package core
+
+import (
+	jsoniter "github.com/json-iterator/go"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestDurationCodec(t *testing.T) {
+	duration := &Duration{Seconds: 10, Nanoseconds: 1000}
+	str, err := jsoniter.ConfigFastest.Marshal(duration)
+	assert.NoError(t, err)
+	assert.Equal(t, "\"10.000001s\"", string(str))
+
+	d := &Duration{}
+	e := jsoniter.ConfigFastest.Unmarshal(str, d)
+	assert.NoError(t, e)
+	assert.Equal(t, int64(10), d.Seconds)
+	assert.Equal(t, int32(1000), d.Nanoseconds)
+}
