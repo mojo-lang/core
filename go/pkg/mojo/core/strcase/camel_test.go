7c1f7948900be2972de66ce59b64bb8c41c4526e
Frankee.zhou <frankee.zhou@gmail.com>
2022-03-15 17:09:44 +0800

    add IrregularCaseRule also suit for the camel conversion

---
+++ go/pkg/mojo/core/strcase/camel_test.go
@@ -0,0 +1,24 @@
+package strcase
+
+import (
+    "github.com/stretchr/testify/assert"
+    "testing"
+)
+
+func TestToCamel(t *testing.T) {
+    RegisterIrregularCaseRule("IPV4", "Ipv4")
+    RegisterIrregularCaseRule("B2B", "B2b")
+
+    for _, c := range cases {
+        assert.Equal(t, c[0], ToCamel(c[1]))
+    }
+}
+
+func TestToLowerCamel(t *testing.T) {
+    RegisterIrregularCaseRule("IPV4", "Ipv4")
+    RegisterIrregularCaseRule("B2B", "B2b")
+
+    for _, c := range cases {
+        assert.Equal(t, c[1], ToLowerCamel(c[0]))
+    }
+}
--- go/pkg/mojo/core/strcase/snake.go
+++ go/pkg/mojo/core/strcase/snake.go
@@ -1,3 +1,43 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Ian Coleman
+ * Copyright (c) 2018 Ma_124, <github.com/Ma124>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, Subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or Substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * from (github.com/iancoleman/strcase)
+ */
+// Copyright 2021 Mojo-lang.org
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package strcase
 
 import (
@@ -10,16 +50,23 @@ func init() {
     RegisterIrregularCaseRule("UInt", "Uint")
 }
 
-type snakeRule struct {
+type caseRule struct {
     *regexp.Regexp
     Replacement string
 }
 
-var irregularSnakeRules []snakeRule
+var irregularSnakeRules []caseRule
 
 func RegisterIrregularCaseRule(rule string, replacement string) {
+    RegisterIrregularSnakeCaseRule(rule, replacement)
+    if reg := regexp.MustCompile(`[a-zA-Z0-9]`); reg.Match([]byte(rule)) {
+        RegisterIrregularCamelCaseRule(replacement, rule)
+    }
+}
+
+func RegisterIrregularSnakeCaseRule(rule string, replacement string) {
     r, _ := regexp.Compile(rule)
-    irregularSnakeRules = append(irregularSnakeRules, snakeRule{
+    irregularSnakeRules = append(irregularSnakeRules, caseRule{
         Regexp:      r,
         Replacement: replacement,
     })
@@ -58,12 +105,16 @@ func ToDelimited(s string, delimiter uint8) string {
     return ToScreamingDelimited(s, delimiter, "", false)
 }
 
-func ToScreamingDelimited(s string, delimiter uint8, ignore string, screaming bool) string {
+func applySnakeRules(s string) string {
     for _, rule := range irregularSnakeRules {
         s = string(rule.Regexp.ReplaceAll([]byte(s), []byte(rule.Replacement)))
     }
+    return s
+}
 
-    s = strings.TrimSpace(s)
+func ToScreamingDelimited(s string, delimiter uint8, ignore string, screaming bool) string {
+    s = applySnakeRules(strings.TrimSpace(s))
+
     n := strings.Builder{}
     n.Grow(len(s) + 2) // nominal 2 bytes of extra space for inserted delimiters
     for i, v := range []byte(s) {
