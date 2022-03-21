52fa6c86dce022e323229639bbd9df4c69a39f4b
Frankee.zhou <frankee.zhou@gmail.com>
2022-03-21 16:30:45 +0800

    update the any test case

--- go/pkg/mojo/core/any.json_test.go
+++ go/pkg/mojo/core/any.json_test.go
@@ -1,15 +1,17 @@
 package core
 
 import (
-	jsoniter "github.com/json-iterator/go"
-	"github.com/stretchr/testify/assert"
-	"testing"
+    jsoniter "github.com/json-iterator/go"
+    "github.com/stretchr/testify/assert"
+    "testing"
 )
 
 func TestAnyCodec_Decode(t *testing.T) {
-	str := `{"@type": "mojo.core.Checksum", "algorithm": "sha256", "val": "werrdsafadfdfdfff"}`
+    str := `{"@type": "mojo.core.Checksum", "algorithm": "sha256", "value": "sha256-checksum-value"}`
 
-	any := &Any{}
-	err := jsoniter.ConfigFastest.Unmarshal([]byte(str), any)
-	assert.NoError(t, err)
+    any := &Any{}
+    err := jsoniter.ConfigFastest.Unmarshal([]byte(str), any)
+    assert.NoError(t, err)
+    assert.Equal(t, Checksum_ALGORITHM_SHA256, any.Get().(*Checksum).Algorithm)
+    assert.Equal(t, "sha256-checksum-value", any.Get().(*Checksum).Value)
 }
