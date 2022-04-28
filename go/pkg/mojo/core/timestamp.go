3dd9c30fe049404b3910caf59fc91411e0bb2fa0
Frankee.zhou <frankee.zhou@gmail.com>
2022-04-28 14:00:08 +0800

    add methods for timestamp

--- go/pkg/mojo/core/timestamp.go
+++ go/pkg/mojo/core/timestamp.go
@@ -11,10 +11,6 @@ func Now() *Timestamp {
     return FromTime(time.Now())
 }
 
-func (x Timestamp) ToTime() time.Time {
-    return time.Unix(x.Seconds, int64(x.Nanoseconds)).In(loc)
-}
-
 func FromTime(t time.Time) *Timestamp {
     seconds := t.Unix()
     return &Timestamp{
@@ -23,8 +19,78 @@ func FromTime(t time.Time) *Timestamp {
     }
 }
 
+func Since(t *Timestamp) *Duration {
+    if t != nil {
+        return FromDuration(time.Since(t.ToTime()))
+    }
+    return nil
+}
+
+func Until(t *Timestamp) *Duration {
+    if t != nil {
+        return FromDuration(time.Until(t.ToTime()))
+    }
+    return nil
+}
+
 func (x *Timestamp) FromTime(t time.Time) {
     ts := FromTime(t)
     x.Seconds = ts.Seconds
     x.Nanoseconds = ts.Nanoseconds
+}
+
+func (x *Timestamp) ToTime() time.Time {
+    return time.Unix(x.Seconds, int64(x.Nanoseconds)).In(loc)
+}
+
+func (x *Timestamp) After(u *Timestamp) bool {
+    if x != nil && u != nil {
+        if x != u {
+            return x.ToTime().After(u.ToTime())
+        }
+    }
+    return false
+}
+
+// Before reports whether the time instant t is before u.
+func (x *Timestamp) Before(u *Timestamp) bool {
+    if x != nil && u != nil {
+        if x != u {
+            return x.ToTime().Before(u.ToTime())
+        }
+    }
+    return false
+}
+
+// Equal reports whether t and u represent the same time instant.
+// Two times can be equal even if they are in different locations.
+// For example, 6:00 +0200 and 4:00 UTC are Equal.
+// See the documentation on the Time type for the pitfalls of using == with
+// Time values; most code should use Equal instead.
+func (x *Timestamp) Equal(u *Timestamp) bool {
+    if x != nil && u != nil {
+        if x == u {
+            return true
+        }
+        return x.ToTime().Equal(u.ToTime())
+    }
+    return false
+}
+
+// Sub returns the duration t-u. If the result exceeds the maximum (or minimum)
+// value that can be stored in a Duration, the maximum (or minimum) duration
+// will be returned.
+func (x *Timestamp) Sub(u *Timestamp) *Duration {
+    if x != nil && u != nil {
+        return FromDuration(x.ToTime().Sub(u.ToTime()))
+    }
+    return nil
+}
+
+func (x *Timestamp) SinceNow() *Duration {
+    return Since(x)
+}
+
+func (x *Timestamp) UntilNow() *Duration {
+    return Until(x)
 }
--- go/pkg/mojo/core/timestamp_test.go
+++ go/pkg/mojo/core/timestamp_test.go
@@ -8,6 +8,7 @@ import (
 
 const (
     TimestampString1       = "2019-10-13T19:52:25+08:00"
+    TimestampString2       = "2019-10-13T19:53:25+08:00"
     TimestampString1Json   = `"2019-10-13T19:52:25+08:00"`
     TimestampString1Number = "1570967545"
     Timestamp1             = int64(1570967545)
@@ -57,4 +58,35 @@ func TestTimestampParse(t *testing.T) {
         assert.Equal(t, Timestamp1, ts.Seconds, "%s seconds not equal", exampleFormats[i])
         assert.Equal(t, int32(0), ts.Nanoseconds, "%s nanos not equal", exampleFormats[i])
     }
+}
+
+func TestTimestamp_Equal(t *testing.T) {
+    t1, _ := ParseTimestamp(TimestampString1)
+    t12, _ := ParseTimestamp(TimestampString1)
+    t2, _ := ParseTimestamp(TimestampString2)
+
+    assert.True(t, t1.Equal(t12))
+    assert.False(t, t1.Equal(t2))
+}
+
+func TestTimestamp_Sub(t *testing.T) {
+    t1, _ := ParseTimestamp(TimestampString1)
+    t2, _ := ParseTimestamp(TimestampString2)
+
+    assert.Equal(t, 1.0, t2.Sub(t1).ToMinutes())
+    assert.Equal(t, -1.0, t1.Sub(t2).ToMinutes())
+}
+
+func TestTimestamp_After(t *testing.T) {
+    t1, _ := ParseTimestamp(TimestampString1)
+    t2, _ := ParseTimestamp(TimestampString2)
+
+    assert.True(t, t2.After(t1))
+}
+
+func TestTimestamp_Before(t *testing.T) {
+    t1, _ := ParseTimestamp(TimestampString1)
+    t2, _ := ParseTimestamp(TimestampString2)
+
+    assert.True(t, t1.Before(t2))
 }
