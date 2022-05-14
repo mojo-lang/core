0253e0924b2ed2773b21919876ce928ececbc95b
Frankee.zhou <frankee.zhou@gmail.com>
2022-05-14 19:24:25 +0800

    fix the http url which has no schema, which will put the host to path.

--- go/pkg/mojo/core/url.fmt.go
+++ go/pkg/mojo/core/url.fmt.go
@@ -1,125 +1,131 @@
 package core
 
 import (
-    "errors"
-    "net/url"
-    "strconv"
-    "strings"
+	"errors"
+	"net/url"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
+var urlSchemaPattern = regexp.MustCompile(`^([a-z0-9+\-.]+://)|mailto:|news:`)
+
 func ParseUrl(raw string) (*Url, error) {
-    url := &Url{}
-    err := url.Parse(raw)
-    if err != nil {
-        return nil, err
-    }
-    return url, nil
+	u := &Url{}
+	err := u.Parse(raw)
+	if err != nil {
+		return nil, err
+	}
+	return u, nil
 }
 
 func (x *Url) Parse(raw string) error {
-    if x == nil {
-        return errors.New("")
-    }
+	if x == nil {
+		return errors.New("")
+	}
 
-    url, err := url.Parse(raw)
-    if err != nil {
-        return err
-    }
+	u, err := url.Parse(raw)
+	if err != nil {
+		return err
+	}
 
-    x.Scheme = url.Scheme
-    x.Authority = &Url_Authority{
-        Host: url.Hostname(),
-    }
+	x.Scheme = u.Scheme
+	x.Authority = &Url_Authority{
+		Host: u.Hostname(),
+	}
 
-    if len(x.Authority.Host) == 0 {
-        url, err = url.Parse("http://" + raw)
-        if err != nil {
-            return err
-        }
-        x.Scheme = ""
-        x.Authority.Host = url.Hostname()
-        if len(x.Authority.Host) == 0 {
-            return errors.New("failed to parse the url")
-        }
-    }
+	if len(x.Authority.Host) == 0 && (len(u.Scheme) > 0 || len(u.Opaque) > 0) && !urlSchemaPattern.MatchString(raw) {
+		u, err = url.Parse("https://" + raw)
+		if err != nil {
+			return err
+		}
+		x.Scheme = ""
+		x.Authority.Host = u.Hostname()
+		if len(x.Authority.Host) == 0 {
+			return errors.New("failed to parse the url")
+		}
+	}
 
-    port := url.Port()
-    if len(port) > 0 {
-        v, err := strconv.Atoi(port)
-        if err != nil {
-            return err
-        }
-        x.Authority.Port = int64(v)
-    }
+	port := u.Port()
+	if len(port) > 0 {
+		v, err := strconv.Atoi(port)
+		if err != nil {
+			return err
+		}
+		x.Authority.Port = int64(v)
+	}
 
-    x.Path = url.Path
-    x.Fragment = url.Fragment
+	x.Path = u.Path
+	x.Fragment = u.Fragment
 
-    x.Query = &Url_Query{
-        Vals: make(map[string]*StringValues),
-    }
-    query := url.Query()
-    for k, v := range query {
-        x.Query.Vals[k] = &StringValues{
-            Vals: v,
-        }
-    }
+	x.Query = &Url_Query{
+		Vals: make(map[string]*StringValues),
+	}
+	query := u.Query()
+	for k, v := range query {
+		x.Query.Vals[k] = &StringValues{
+			Vals: v,
+		}
+	}
 
-    return nil
+	return nil
 }
 
 func (x *Url) Format() string {
-    if x == nil {
-        return ""
-    }
+	if x == nil {
+		return ""
+	}
 
-    host := ""
-    if x.Authority != nil {
-        host = x.Authority.Host
-        if x.Authority.Port > 0 {
-            host = host + ":" + strconv.FormatInt(x.Authority.Port, 10)
-        }
-    }
+	host := ""
+	if x.Authority != nil {
+		host = x.Authority.Host
+		if x.Authority.Port > 0 {
+			host = host + ":" + strconv.FormatInt(x.Authority.Port, 10)
+		}
+	}
 
-    u := url.URL{
-        Scheme:   x.Scheme,
-        Host:     host,
-        Path:     x.Path,
-        Fragment: x.Fragment,
-    }
+	u := url.URL{
+		Scheme:   x.Scheme,
+		Host:     host,
+		Path:     x.Path,
+		Fragment: x.Fragment,
+	}
 
-    if x.Query != nil {
-        query := url.Values{}
-        for k, v := range x.Query.Vals {
-            if v != nil {
-                query[k] = v.Vals
-            }
-        }
-        u.RawQuery = query.Encode()
-    }
+	if x.Query != nil {
+		query := url.Values{}
+		for k, v := range x.Query.Vals {
+			if v != nil {
+				query[k] = v.Vals
+			}
+		}
+		u.RawQuery = query.Encode()
+	}
 
-    return u.String()
+	if len(u.Scheme) > 0 {
+		return u.String()
+	} else {
+		return strings.TrimPrefix(u.String(), "//")
+	}
 }
 
 func (x *Url) ToString() string {
-    return x.Format()
+	return x.Format()
 }
 
 //FormatWithoutSchema get url like: "apis.company.com/path/to/resource"
 func (x *Url) FormatWithoutSchema() string {
-    if x == nil {
-        return ""
-    }
+	if x == nil {
+		return ""
+	}
 
-    url := &Url{
-        Authority: &Url_Authority{
-            Host: x.GetAuthority().GetHost(),
-            Port: x.GetAuthority().GetPort(),
-            //UserInfo: x.GetAuthority().GetUserInfo(),
-        },
-        Path:     x.Path,
-        Query:    x.Query,
-        Fragment: x.Fragment,
-    }
-    return strings.TrimPrefix(url.Format(), "//")
+	u := &Url{
+		Authority: &Url_Authority{
+			Host: x.GetAuthority().GetHost(),
+			Port: x.GetAuthority().GetPort(),
+		},
+		Path:     x.Path,
+		Query:    x.Query,
+		Fragment: x.Fragment,
+	}
+	return strings.TrimPrefix(u.Format(), "//")
 }
