170b8abd689a538827ad4fd6a9c2549234aa5ba6
Frankee.zhou <frankee.zhou@gmail.com>
2023-07-14 11:58:02 +0800

    add more common MediaType name, and add IsSame method for it

--- go/pkg/mojo/core/media_type.go
+++ go/pkg/mojo/core/media_type.go
@@ -12,9 +12,31 @@ const (
 	ApplicationGeoJson           = "application/geo+json"
 	ApplicationZip               = "application/zip"
 
-	ImagePng  = "image/png"
+	AudioAAC    = "audio/aac"
+	AudioMP4    = "audio/mp4"
+	AudioMPEG   = "audio/mpeg"
+	AudioOgg    = "audio/ogg"
+	AudioVorbis = "audio/vorbis"
+	AudioWave   = "audio/wave"
+	AudioWebm   = "audio/webm"
+
+	FontTTF  = "font/ttf"
+	FontOTF  = "font/otf"
+	FontWOFF = "font/woff"
+
+	ImageGif  = "image/gif"
 	ImageJpeg = "image/jpeg"
+	ImagePng  = "image/png"
 	ImageSvg  = "image/svg+xml"
+	ImageWebp = "image/webp"
+
+	ModelGltfBinary = "model/gltf-binary"
+	ModelGltfJson   = "model/gltf+json"
+	ModelOBJ        = "model/obj"
+	ModelU3D        = "model/u3d"
+
+	MultipartFormData   = "multipart/form-data"
+	MultipartByteRanges = "multipart/byteranges"
 
 	TextPlain      = "text/plain"
 	TextCsv        = "text/csv"
@@ -22,5 +44,14 @@ const (
 	TextHtml       = "text/html"
 	TextJavascript = "text/javascript"
 
-	VideoMp4 = "video/mp4"
+	VideoMp4  = "video/mp4"
+	VideoOgg  = "video/ogg"
+	VideoWebm = "video/webm"
 )
+
+func (x *MediaType) IsSame(mediaType string) bool {
+	if x != nil {
+		return x.Type+"/"+x.Subtype == mediaType
+	}
+	return false
+}
