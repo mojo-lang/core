2a3b2cc32c9e6f21e8b08019cf1fc903199bf883
Frankee.zhou <frankee.zhou@gmail.com>
2023-10-30 10:12:19 +0800

    move the MkDir which os specified to different build files.

---
+++ go/pkg/mojo/core/files_windows.go
@@ -0,0 +1,9 @@
+package core
+
+// MkDir create dir with recursively, with the perm but no os mask
+func MkDir(filePath string, perm os.FileMode) error {
+	if !IsExist(filePath) {
+		return os.MkdirAll(filePath, perm)
+	}
+	return nil
+}
